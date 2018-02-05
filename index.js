9838f70541cebe87dd56d0de757ea27d4f34eda9
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-02-05 15:51:00 +0900

    no message

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const medias = {audio : false, video : {
-        facingMode: "user"
-      }},
-      video  = document.getElementById("video");
+  facingMode : {
+    exact : "environment"
+  }
+}},
+video  = document.getElementById("video"),
+canvas = document.getElementById("canvas"),
+ctx    = canvas.getContext("2d");
 
 navigator.getUserMedia(medias, successCallback, errorCallback);
 
+requestAnimationFrame(draw);
+
 function successCallback(stream) {
-  video.srcObject = stream;
+video.srcObject = stream;
 };
 
 function errorCallback(err) {
-  alert(err);
+alert(err);
 };
+
+function draw() {
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.drawImage(video, 0, 0);
+
+requestAnimationFrame(draw);
+}
