76e1f7975b61779a8f2a62fa48d992e16eb25f83
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-02-06 12:17:54 +0900

    barcode

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
-  var video = document.getElementById('video');
-  var localStream = null;
+var video = document.getElementById('video');
+var localStream = null;
 
-  // リアカメラを使用.
-  //navigator.getUserMedia({video: true, audio: false},
-  navigator.getUserMedia(
-      {
-          audio: false,
-          video: { facingMode: { exact: "environment" } }
-      },
-      function(stream) {
-          // mobile safariでは動作しない.
-          //window.URL = window.URL || window.webkitURL;
-          //video.src = window.URL.createObjectURL(stream);
-          video.srcObject = stream;
-          localStream = stream;
-      },
-      function(err) {
-          console.log(err);
-      }
-  );
+// リアカメラを使用.
+//navigator.getUserMedia({video: true, audio: false},
+navigator.getUserMedia(
+    {
+        audio: false,
+        video: { facingMode: { exact: "environment" } }
+    },
+    function(stream) {
+        // mobile safariでは動作しない.
+        //window.URL = window.URL || window.webkitURL;
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        localStream = stream;
+    },
+    function(err) {
+        console.log(err);
+    }
+);
 
-  function decodeImageFromBase64(data, callback){
-      qrcode.callback = callback;
-      qrcode.decode(data)
-  }
-
-
-
+function decodeImageFromBase64(data, callback){
+    qrcode.callback = callback;
+    qrcode.decode(data)
+}
 
   
 function loop(){
-
 // document.getElementById("action").addEventListener('click', function() {
   if(localStream) {
     var canvas = document.getElementById('canvas');
@@ -62,8 +58,84 @@ function loop(){
 // },false);
 
 
+class BarcodeReader {
+  constructor() {
+    Quagga.onProcessed(this._onProcessed.bind(this));
+    Quagga.onDetected(this._onDetected.bind(this));
+  }
 
-  // console.log('loop!');
+  get config() {
+    return {
+      inputStream: {
+        target: '#preview',
+        size: 640,
+        singleChannel: false
+      },
+      locator: {
+        patchSize: "medium",
+        halfSample: true
+      },
+      decoder: {
+        readers: [{
+          format: "code_128_reader",
+          config: {}
+        }]
+      },
+      numOfWorker: navigator.hardwareConcurrency || 1,
+      locate: true,
+      src: null
+    };
+  }
+
+  
+  decode(src) {
+    const config = Object.assign({}, this.config, { src: src });
+
+    return new Promise((resolve, reject) => {
+      Quagga.decodeSingle(config, result => {
+        resolve(result);
+      });
+    })
+  }
+
+  /** 処理が完了したときに実行される*/
+  _onProcessed(data) {
+    console.log('onProccessed', data);
+    const ctx = Quagga.canvas.ctx.overlay;
+    const canvas = Quagga.canvas.dom.overlay;
+		if (!data) { return; }
+    // 認識したバーコードを囲む
+    if (data.boxes) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      const hasNotRead = box => box !== data.box;
+      data.boxes.filter(hasNotRead).forEach(box => {
+        Quagga.ImageDebug.drawPath(box, {x:0, y:1}, ctx, {color: 'green', lineWidth: 2});
+      });
+    }
+    if (data.box) {
+      Quagga.ImageDebug.drawPath(data.box, {x:0, y:1}, ctx, {color: 'blue', lineWidth: 2});
+    }
+    if (data.codeResult && data.codeResult.code) {
+      Quagga.ImageDebug.drawPath(data.line, {x:'x', y:'y'}, ctx, {color: 'red', lineWidth: 3});
+    }
+  }
+
+  /**
+   * バーコード読み取りが成功したときに実行される
+   */
+  _onDetected(data) {
+    alert(data);
+  }
+  
+  getDataURL() {
+    return Quagga.canvas.dom.image.toDataURL();
+  }
+}
+
 }
 
 setInterval(loop,200);
+
+
+
+
