331d3387aeee0d9a555bab0433661dffeb8ddd29
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-02-07 10:48:30 +0900

    no message

--- barcode-reader.js
+++ barcode-reader.js
@@ -1,4 +1,3 @@
-
 Quagga.init({
   inputStream : {
     name : "Live",
@@ -14,10 +13,10 @@ Quagga.init({
       patchSize: "medium",
       halfSample: true
     },
-
   },
   decoder : {
-    readers : ["ean_reader"]
+    // readers : ["ean_reader"]
+    readers : ["code_128_reader"]    
   }
 }, function(err) {
   if (err) {
@@ -28,90 +27,34 @@ Quagga.init({
   Quagga.start();
 });
 
-
 // Make sure, QuaggaJS draws frames an lines around possible 
 // barcodes on the live stream
 Quagga.onProcessed(function(result) {
   var drawingCtx = Quagga.canvas.ctx.overlay,
-    drawingCanvas = Quagga.canvas.dom.overlay;
+  drawingCanvas = Quagga.canvas.dom.overlay;
 
   if (result) {
-    if (result.boxes) {
-      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-      result.boxes.filter(function (box) {
-        return box !== result.box;
-      }).forEach(function (box) {
-        Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-      });
-    }
+    // if (result.boxes) {
+    //   drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+    //   result.boxes.filter(function (box) {
+    //     return box !== result.box;
+    //   }).forEach(function (box) {
+    //     Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+    //   });      
+    // }
 
     if (result.box) {
+      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+      
       Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
     }
-
+    // 読み取り結果
     if (result.codeResult && result.codeResult.code) {
       Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-    }
-
-
-
-    if (!result.codeResult) {
-      
-    }else{
       console.log(result.codeResult.code);
       alert(result.codeResult.code);
     }
 
-
-
-
   }
 
- 
-
-
 });
-
-
-
-
-
-
-
-
-// Quagga.decodeSingle({
-//   decoder: {
-//       readers: ["code_128_reader"] // List of active readers
-//   },
-//   locate: true, // try to locate the barcode in the image
-//   src: 'img/code_128.gif' // or 'data:image/jpg;base64,' + data
-// }, function(result){
-
-//   if(result.codeResult) {
-//       console.log("result", result.codeResult.code);
-//   } else {
-//       console.log("not detected");
-//   }
-// });
-
-
-
-// onload = function() {
-//   draw();
-// };
-// function draw() {
-//   var canvas = document.getElementById('c1');
-//   if ( ! canvas || ! canvas.getContext ) { return false; }
-//   var ctx = canvas.getContext('2d');
-//   /* Imageオブジェクトを生成 */
-//   var img = new Image();
-//   img.src = "img/code_39.png?" + new Date().getTime();
-//   /* 画像が読み込まれるのを待ってから処理を続行 */
-//   img.onload = function() {
-//     ctx.drawImage(img, 0, 0);
-//   }
-// }
-
-
-
-
--- qr-reader.js
+++ qr-reader.js
@@ -3,7 +3,6 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
   var localStream = null;
 
   // リアカメラを使用.
-  //navigator.getUserMedia({video: true, audio: false},
   navigator.getUserMedia(
     {
       audio: false,
@@ -20,55 +19,35 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
       console.log(err);
     }
   );
-
   function decodeImageFromBase64(data, callback){
     qrcode.callback = callback;
     qrcode.decode(data)
   }
-
-
-
-
   
 function loop(){
-
-// document.getElementById("action").addEventListener('click', function() {
   if(localStream) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     var img = document.getElementById('img');
-
     //videoの縦幅横幅を取得
     // var w = video.offsetWidth;
     var w = video.offsetWidth/1.8; //縦横比修正
     var h = video.offsetHeight;
-
     //同じサイズをcanvasに指定
     canvas.setAttribute("width", w);
     canvas.setAttribute("height", h);
-
-
     //canvasにコピー
     ctx.drawImage(video, 0, 0, w, h);
 
     decodeImageFromBase64(canvas.toDataURL('image/png'), function(result) {
+      // エラーを除きたい
       if(result == "error decoding QR Code"){
-
       }else{
-        // alert(result);
-
-
         var e = document.getElementById('result');
         e.textContent = result;
       }          
-      
     });
   };
-// },false);
-
-
-
-  // console.log('loop!');
 }
 
 setInterval(loop,200);
