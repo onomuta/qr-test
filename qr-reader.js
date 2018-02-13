dcb44a90cd52059aeaf5d724deb4f3ad65fa476b
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-02-13 12:52:13 +0900

    no message

--- qr-reader.js
+++ qr-reader.js
@@ -1,34 +1,29 @@
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
-  var video = document.getElementById('video');
-  var localStream = null;
-
-  // リアカメラを使用.
-  navigator.getUserMedia(
-    {
-      audio: false,
-      video: { facingMode: { exact: "environment" } }
-    },
-    function(stream) {
-      // mobile safariでは動作しない.
-      //window.URL = window.URL || window.webkitURL;
-      //video.src = window.URL.createObjectURL(stream);
-      video.srcObject = stream;
-      localStream = stream;
-    },
-    function(err) {
-      console.log(err);
-    }
-  );
-  function decodeImageFromBase64(data, callback){
-    qrcode.callback = callback;
-    qrcode.decode(data)
+var video = document.getElementById('video');
+var localStream = null;
+// リアカメラを使用.
+navigator.getUserMedia(
+  {
+    audio: false,
+    video: { facingMode: { exact: "environment" } }
+  },
+  function(stream) {
+    video.srcObject = stream;
+    localStream = stream;
+  },
+  function(err) {
+    console.log(err);
   }
+);
+function decodeImageFromBase64(data, callback){
+  qrcode.callback = callback;
+  qrcode.decode(data)
+}
   
 function loop(){
   if(localStream) {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    var img = document.getElementById('img');
     //videoの縦幅横幅を取得
     // var w = video.offsetWidth;
     var w = video.offsetWidth/1.8; //縦横比修正
@@ -40,7 +35,7 @@ function loop(){
     ctx.drawImage(video, 0, 0, w, h);
 
     decodeImageFromBase64(canvas.toDataURL('image/png'), function(result) {
-      // エラーを除きたい
+      // エラーを除きたい..
       if(result == "error decoding QR Code"){
       }else{
         var e = document.getElementById('result');
@@ -49,5 +44,5 @@ function loop(){
     });
   };
 }
-
+//0.2秒ごとに読み込み
 setInterval(loop,200);
