ce09b23ba560831b6eb16636c415a3790b13231a
Takao Onomura <takao.onomura@kameda-hi.co.jp>
2018-05-25 14:54:57 +0900

    読み込み範囲指定

--- barcode-reader.js
+++ barcode-reader.js
@@ -13,11 +13,22 @@ Quagga.init({
       patchSize: "medium",
       halfSample: true
     },
+    numOfWorkers:10,
+    // frequency: 10,
+    area: { // defines rectangle of the detection/localization area
+      top: "30%",    // top offset
+      right: "10%",  // right offset
+      left: "10%",   // left offset
+      bottom: "30%"  // bottom offset
+    },
+    // singleChannel: false // true: only the red color-channel is read
+
   },
   decoder : {
     //ここで読み込むバーコードの種類を指定。（複数可）
-    readers : ["ean_reader"]
+    readers : ["ean_reader"],
     // readers : ["code_128_reader"]    
+    // multiple:false
   }
 }, function(err) {
   if (err) {
@@ -34,16 +45,20 @@ Quagga.onProcessed(function(result) {
   var drawingCtx = Quagga.canvas.ctx.overlay,
   drawingCanvas = Quagga.canvas.dom.overlay;
 
+  var canvasWidth = drawingCanvas.getAttribute("width");
+  var canvasHeight = drawingCanvas.getAttribute("height");
+
+  
   if (result) {
-    //　バーコードっぽいところに緑色の枠を出す
-    // if (result.boxes) {
-    //   drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-    //   result.boxes.filter(function (box) {
-    //     return box !== result.box;
-    //   }).forEach(function (box) {
-    //     Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-    //   });      
-    // }
+    // 　バーコードっぽいところに緑色の枠を出す
+    if (result.boxes) {
+      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+      result.boxes.filter(function (box) {
+        return box !== result.box;
+      }).forEach(function (box) {
+        Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+      });      
+    }
 
     // 読み込めたバーコードに青い枠を出す
     if (result.box) {
@@ -56,7 +71,26 @@ Quagga.onProcessed(function(result) {
       console.log(result.codeResult.code);
       alert(result.codeResult.code);
     }
-
   }
 
+  var c=document.getElementById("drawingBuffer");
+  var ctx=c.getContext("2d");
+
+  drawingCtx.beginPath();
+  drawingCtx.lineWidth="4";
+  drawingCtx.strokeStyle="#ffcc00";
+  
+  // drawingCtx.rect(canvasWidth/2 - 200 ,canvasHeight/2 -70, 400, 140);
+
+  drawingCtx.moveTo(0          , canvasHeight/3);
+  drawingCtx.lineTo(canvasWidth, canvasHeight/3);
+
+  drawingCtx.moveTo(0          , canvasHeight - canvasHeight/3);
+  drawingCtx.lineTo(canvasWidth, canvasHeight - canvasHeight/3);
+
+
+  drawingCtx.stroke();
+
 });
+
+
